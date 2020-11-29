33bdb09461ddd610c9fc282ae849dbc1668974bb
Thomas Coats <github@voodoolabs.net>
2020-11-29 21:22:59 +1300

    Working?

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const inject = require('seacreature/lib/inject')
 const { parentPort } = require('worker_threads')
 
-inject('pod', async ({ app, hub, log }) => {
+const history_lines = process.env.HISTORY_LINES
+  ? Number(process.env.HISTORY_LINES)
+  : 10
+
+inject('pod', async ({ app, hub, log, startup }) => {
+  const logs = new Map()
+
   app.use(async (req, res, next) => {
     req.token = null
     const token = req.headers['x-access-token']
@@ -16,7 +22,34 @@ inject('pod', async ({ app, hub, log }) => {
       const { e, p } = JSON.parse(msg)
       await hub.emit(e, p)
     })
-  app.get('/', inject.one('req.token'), (req, res) => {
-    res.send('ok')
+
+  app.get('/logs', inject.one('req.token'), (req, res) => {
+    if (req.query.app) {
+      if (!logs.has(req.query.app)) return res.send([])
+      return res.send(logs.get(req.query.app))
+    }
+    res.send(Array.from(logs.entries()))
   })
+
+  ;(async () => {
+    await startup.released()
+    parentPort.postMessage(JSON.stringify({ e: 'subscribe_stdout'}))
+    parentPort.postMessage(JSON.stringify({ e: 'subscribe_stderr'}))
+  })()
+
+  const add = (level, spec, data) => {
+    if (data.length > 0 && data[0] == '{') data = JSON.parse(data)
+    if (!logs.has(spec.name)) logs.set(spec.name, [])
+    const lines = logs.get(spec.name)
+    lines.push({
+      level,
+      ts: new Date().getTime() / 1000,
+      data
+    })
+    if (lines.length > history_lines)
+      logs.set(spec.name, lines.slice(-history_lines))
+  }
+
+  hub.on('worker.stdout', ({ spec, data }) => add('info', spec, data))
+  hub.on('worker.stderr', ({ spec, data }) => add('error', spec, data))
 })
